113a86bf899917d8abb5800253d1758a4302cbb5
regis <regiscorblin@yahoo.fr>
2021-12-12 16:49:25 +0100

    Update postgres_util with call to django api and permission on isAuthenticated

--- postgres_util.py
+++ postgres_util.py
@@ -5,25 +5,37 @@ import requests
 
 new_tree = {'position': '5-5','tag':'P1','tree_type':'Pommier','comments':'No comments','pictures':'http://test.com'}
 
+def login_and_get_token(username:str, password:str)->str:
+    URL1='http://127.0.0.1:8000/accounts/login/'
+    client = requests.session()
+    # Retrieve the CSRF token first
+    client.get(URL1)  # sets the cookie
+    # login to app and get updated CSRF token
+    login_data = dict(username=username, password=password, csrfmiddlewaretoken=client.cookies['csrftoken'])
+    client.post(URL1, data=login_data)
+    return client
+
+with open("config.json",'r') as f:
+    data = json.load(f)
+    username = data[0]['auth']['username']
+    password = data[0]['auth']['password']
+
+client = login_and_get_token(username,password)
+csrftoken = client.cookies['csrftoken']
+
 def get_all_trees()->json:
-    response = requests.get('http://127.0.0.1:8000/treeapi/trees/')
+    response = client.get('http://127.0.0.1:8000/treeapi/trees/', headers={"X-CSRFToken":csrftoken})
     return response.json()
 
 def get_tree_from_tag(tag:str)->json:
     data={'tag':tag}
-    response = requests.get('http://127.0.0.1:8000/treeapi/trees/', params=data)
+    response = client.get('http://127.0.0.1:8000/treeapi/trees/', params=data, headers={"X-CSRFToken":csrftoken})
     return response.json()
 
 def update_tree(tag:str,new_values:Dict):
     id = get_tree_from_tag(tag)[0]['id']
     data_patch = new_values
-    response = requests.patch(f'http://127.0.0.1:8000/treeapi/trees/{str(id)}/',data=data_patch)
+    response = client.patch(f'http://127.0.0.1:8000/treeapi/trees/{str(id)}/', data=data_patch, headers={"X-CSRFToken":csrftoken})
 
 def create_tree(values:Dict):
-    response = requests.post('http://127.0.0.1:8000/treeapi/trees/', values)
-
-
-#print(get_all_trees())
-#print(get_tree_from_tag('P1'))
-#update_tree('P1',{'tag':'P1','tree_type':'Fraise','comments':'youpi'})
-#create_tree(new_tree)
+    response = client.post('http://127.0.0.1:8000/treeapi/trees/', values, headers={"X-CSRFToken":csrftoken})
