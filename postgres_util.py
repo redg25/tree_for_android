c5de547f036a2f654752e64905f2daa8b5f845a6
regis <regiscorblin@yahoo.fr>
2021-12-16 13:27:31 +0100

    Use prod app

--- postgres_util.py
+++ postgres_util.py
@@ -6,7 +6,7 @@ import requests
 new_tree = {'position': '5-5','tag':'P1','tree_type':'Pommier','comments':'No comments','pictures':'http://test.com'}
 
 def login_and_get_token(username:str, password:str)->str:
-    URL1='http://127.0.0.1:8000/accounts/login/'
+    URL1='http://farmcedric.herokuapp.com/accounts/login/'
     client = requests.session()
     # Retrieve the CSRF token first
     client.get(URL1)  # sets the cookie
@@ -24,18 +24,19 @@ client = login_and_get_token(username,password)
 csrftoken = client.cookies['csrftoken']
 
 def get_all_trees()->json:
-    response = client.get('http://127.0.0.1:8000/treeapi/trees/', headers={"X-CSRFToken":csrftoken})
+    response = client.get('http://farmcedric.herokuapp.com/treeapi/trees/', headers={"X-CSRFToken":csrftoken})
+    #response = client.get('http://127.0.0.1:8000/treeapi/trees/', headers={"X-CSRFToken":csrftoken})
     return response.json()
 
 def get_tree_from_tag(tag:str)->json:
     data={'tag':tag}
-    response = client.get('http://127.0.0.1:8000/treeapi/trees/', params=data, headers={"X-CSRFToken":csrftoken})
+    response = client.get('http://farmcedric.herokuapp.com/treeapi/trees/', params=data, headers={"X-CSRFToken":csrftoken})
     return response.json()
 
 def update_tree(tag:str,new_values:Dict):
     id = get_tree_from_tag(tag)[0]['id']
     data_patch = new_values
-    response = client.patch(f'http://127.0.0.1:8000/treeapi/trees/{str(id)}/', data=data_patch, headers={"X-CSRFToken":csrftoken})
+    response = client.patch(f'http://farmcedric.herokuapp.com/treeapi/trees/{str(id)}/', data=data_patch, headers={"X-CSRFToken":csrftoken})
 
 def create_tree(values:Dict):
-    response = client.post('http://127.0.0.1:8000/treeapi/trees/', values, headers={"X-CSRFToken":csrftoken})
+    response = client.post('http://farmcedric.herokuapp.com//treeapi/trees/', values, headers={"X-CSRFToken":csrftoken})
